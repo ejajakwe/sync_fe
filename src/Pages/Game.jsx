672238e5c2b2e3402c2608b4c80c0ebc1002740f
Eja <ejajakwe@gmail.com>
2025-08-14 20:02:41 +0700

    test game

--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -15,9 +15,11 @@ import {
 export default function Game() {
   const [games, setGames] = useState([]);
 
+  const API_BASE = import.meta.env.VITE_API_BASE;
+
   useEffect(() => {
     // Fetch data dari API Laravel
-    fetch("http://127.0.0.1:8000/api/games")
+    fetch("${API_BASE}/api/games")
       .then((res) => {
         if (!res.ok) {
           throw new Error("Gagal mengambil data game");
@@ -39,29 +41,29 @@ export default function Game() {
       <div className="max-w-7xl mx-auto px-4 py-10">
         <h1 className="text-xl font-bold mb-6 lg:px-8">Pilih Game</h1>
 
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 lg:px-8">
-            {games.map((game) => (
-              <Link to={`/game/${game.slug}`} key={game.id}>
-                <Card className="hover:shadow-lg transition-shadow bg-gray-200 border-none h-[201px] flex flex-col justify-between">
-                  <CardContent className="p-0">
-                    <img
-                      src={game.image_url} 
-                      alt={game.name}
-                      className="w-full h-[140px] object-cover rounded-t-md"
-                    />
-                  </CardContent>
-                  <CardHeader className="px-4 py-3">
-                    <CardTitle className="text-sm leading-tight text-black">
-                      {game.name}
-                    </CardTitle>
-                    <CardDescription className="text-xs text-gray-600">
-                      {game.publisher}
-                    </CardDescription>
-                  </CardHeader>
-                </Card>
-              </Link>
-            ))}
-          </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6 lg:px-8">
+          {games.map((game) => (
+            <Link to={`/game/${game.slug}`} key={game.id}>
+              <Card className="hover:shadow-lg transition-shadow bg-gray-200 border-none h-[201px] flex flex-col justify-between">
+                <CardContent className="p-0">
+                  <img
+                    src={game.image_url}
+                    alt={game.name}
+                    className="w-full h-[140px] object-cover rounded-t-md"
+                  />
+                </CardContent>
+                <CardHeader className="px-4 py-3">
+                  <CardTitle className="text-sm leading-tight text-black">
+                    {game.name}
+                  </CardTitle>
+                  <CardDescription className="text-xs text-gray-600">
+                    {game.publisher}
+                  </CardDescription>
+                </CardHeader>
+              </Card>
+            </Link>
+          ))}
+        </div>
       </div>
 
       <Footer />
